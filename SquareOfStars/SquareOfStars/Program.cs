10b4d9094a28fddb799714be970d6378071ddc6a
steffanyS <stefani.sabcheva@gmail.com>
2017-01-17 14:28:54 +0200

    all the exercises added

---
+++ SquareOfStars/SquareOfStars/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            for (int j = 0; j < n - 2; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (i == 0 || i == n - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var number1 = int.Parse(textBox1.Text);
+            var number2 = int.Parse(textBox2.Text);
+            var result = number1 + number2;
+            textBox3.Text = result.ToString();
+
+        }
+    }
+}
